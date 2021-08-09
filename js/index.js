e9895f2be7799656dae7e779054d9811cf0a6f07
Bevis <bevis.jhang@hy-tech.com.tw>
2021-08-09 20:37:41 +0800

    修正動畫

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
-
 window.onload = function () {
-    document.getElementById("arrow-left").addEventListener("click", moveCarouselToLeft)
-    document.getElementById("arrow-right").addEventListener("click", moveCarouselToRight)
+  document
+    .getElementById("arrow-left")
+    .addEventListener("click", moveCarouselToLeft)
+  document
+    .getElementById("arrow-right")
+    .addEventListener("click", moveCarouselToRight)
+  window.addEventListener("scroll", onScroll)
 
-    function moveCarouselToLeft() {
-        moveCarousel(-300)
-    }
+  function moveCarouselToLeft() {
+    moveCarousel(-300)
+  }
 
-    function moveCarouselToRight() {
-        moveCarousel(300)
-    }
+  function moveCarouselToRight() {
+    moveCarousel(300)
+  }
 
-    function moveCarousel(px) {
-        document.getElementById("carousel").scrollLeft += px
+  function moveCarousel(px) {
+    document.getElementById("carousel").scrollLeft += px
+  }
+
+  function onScroll(event) {
+    if (window.scrollY > 1100) {
+      document.getElementById("activity_card").classList.remove("hidden")
     }
+  }
 }
